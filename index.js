c090b70724776b886af358d598c7cad93e168d63
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-04-06 19:05:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+PORT= 3000
+
+app.get('/',(req, res) => (res.send('PROYECTO BACK')))
+
+
+
+
+
+app.listen(PORT, () => {
+
+    console.log(`Express esta escuchando en el puerto http://localhost:${PORT}`)
+
+
+
+})
